ea1f16e396d7805a55f79054218f5f11d49e5a8e
Brett Norris <brettjnorris@gmail.com>
2021-12-29 01:46:52 -0500

    TAY, TXA, TYA

--- src/cpu.rs
+++ src/cpu.rs
@@ -119,8 +119,11 @@ impl CPU {
                     self.sty(&opcode.mode);
                 }
 
-                0xAA => self.tax(),
-                0xE8 => self.inx(),
+                0xaa => self.tax(),
+                0xa8 => self.tay(),
+                0x8a => self.txa(),
+                0x98 => self.tya(),
+                0xe8 => self.inx(),
                 0x00 => return,
                 _ => todo!()
             }
@@ -158,6 +161,21 @@ impl CPU {
     fn tax(&mut self) {
         self.register_x = self.register_a;
         self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    fn tay(&mut self) {
+        self.register_y = self.register_a;
+        self.update_zero_and_negative_flags(self.register_y);
+    }
+
+    fn txa(&mut self) {
+        self.register_a = self.register_x;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn tya(&mut self) {
+        self.register_a = self.register_y;
+        self.update_zero_and_negative_flags(self.register_a);
     }
 
     fn inx(&mut self) {
@@ -265,6 +283,30 @@ mod test {
         cpu.load_and_run(vec![0xa9, 0x0A, 0xaa, 0x00]);
 
         assert_eq!(cpu.register_x, 10);
+    }
+
+    #[test]
+    fn test_0xa8_tay_move_a_to_y() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa9, 0x0A, 0xa8, 0x00]);
+
+        assert_eq!(cpu.register_y, 10);
+    }
+
+    #[test]
+    fn test_0x8a_txa_move_x_to_a() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa2, 0x0a, 0x8a, 0x00]);
+
+        assert_eq!(cpu.register_a, 10)
+    }
+
+    #[test]
+    fn test_0x98_tya_move_y_to_a() {
+        let mut cpu = CPU::new();
+        cpu.load_and_run(vec![0xa0, 0x0a, 0x98, 0x00]);
+
+        assert_eq!(cpu.register_a, 10)
     }
 
     #[test]
